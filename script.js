9cd310255f0409f94e44c0fe1ea2573f31653b7b
Muhammed Aslam Shah <muhammedaslamshah700@gmail.com>
2024-12-01 13:05:55 +0530

    complete 1st task

---
+++ script.js
@@ -0,0 +1,15 @@
+let quiData = [
+  {
+    question : "Most popular programming language ?",
+    a: "C++",
+    b: "Python",
+    c: "JavaScript",
+    answer: "c"
+  },
+]
+
+// Get element from DOM
+let title = document.querySelector('.title');
+
+// Task 1
+//  Display UI
